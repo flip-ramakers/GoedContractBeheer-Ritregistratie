47a1a636d2614211eb331a85382728d0cbd98336
FlipRamakers <124157012+FlipRamakers@users.noreply.github.com>
2025-03-31 11:08:37 +0200

    favicon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+<link rel="icon" type="image/png" href="{{ asset('images/Goedcontractbeheerfavicon.png') }}">
 
 <body>
     @include('layouts.header')

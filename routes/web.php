ca67cd3d98c8c2a2c4b226bfb928465a315b18cb
Jop Mesman <jopmesman@hotmail.com>
2025-06-02 20:50:01 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [MobileLoginController::class, 'showLoginform'])->name('login.show');
 
 Route::prefix('admin')->group(function () {
-    Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['auth'],], function () {
         Route::resource('admins', AdminController::class);
         Route::get('admins/{admin}/edit', [AdminController::class, 'edit'])->name('admins.edit');
 
@@ -28,8 +28,9 @@ Route::prefix('admin')->group(function () {
         Route::post('/logout', [LoginController::class, 'logout'])->name('logoutadmin');
 
         Route::get('rides', [RideController::class, 'index'])->name('rides.index');
-        Route::get('rides/{ride}', [RideController::class, 'show'])->name('rides.show');
-        Route::delete('rides/{ride}', [RideController::class, 'destroy'])->name('rides.destroy');
+        Route::post('rides/export', [RideController::class, 'export'])->name('admin.rides.export');
+        Route::get('rides/{ride}', [RideController::class, 'show'])->name('admin.rides.show');
+        Route::delete('rides/{ride}', [RideController::class, 'destroy'])->name('admin.rides.destroy');
     });
 
     Route::get('/login', [LoginController::class, 'show'])->name('login.show');
@@ -41,14 +42,14 @@ Route::prefix('chauffeur')->group(function () {
     Route::group(['middleware' => ['auth:chauffeur']], function () {
         Route::match(['get', 'post'], '/clienten', [MobileClientenController::class, 'index'])->name('chauffeur.clienten');
         Route::post('/mobile-client', [MobileClientenController::class, 'show'])->name('chauffeur.clienten.show');
-        
+
         Route::get('/mobile-client/{client}', [MobileClientenController::class, 'show'])->name('mobile-client.show');
-        
+
         Route::post('/rides', [RideController::class, 'store'])->name('rides.store');
-        
+
         Route::get('/rides/{ride}', [RideController::class, 'show'])->name('rides.show');
     });
-    
+
     Route::get('login', [MobileLoginController::class, 'showLoginform'])->name('login.show');
 
     Route::post('login', [MobileLoginController::class, 'login'])->name('login');

e2a7c0fb2f8bf97066376cac678c0fd854bb3cdd
Jop Mesman <jopmesman@hotmail.com>
2025-06-02 21:02:38 +0200

    Update index.blade.php

--- resources/views/rides/index.blade.php
+++ resources/views/rides/index.blade.php
@@ -3,6 +3,10 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-center mb-4" style="margin-top:25px;">
         <h2 class="mb-0">{{ __('labels.rides') }}</h2>
+        <form action="{{ route('admin.rides.export') }}" method="POST">
+            @csrf
+            <button type="submit" class="btn btn-info">{{ __('labels.export') }}</button>
+        </form>
     </div>
 
     <table class="table table-bordered">
@@ -21,15 +25,21 @@
             @foreach ($rides as $ride)
                 <tr>
                     <td>{{ $ride->id }}</td>
-                    <td>{{ $ride->client->name }}</td>
-                    <td>{{ $ride->daycare->name ?? '-' }}</td>
+                    <td>
+                        {{ $ride->client->name }}<br />
+                        {!! nl2br(e($ride->client->address)) !!}
+                    </td>
+                    <td>
+                        {{ $ride->daycare->name ?? '-' }}<br />
+                        {!! nl2br(e($ride->daycare->address)) !!}
+                    </td>
                     <td>{{ __('labels.' . $ride->status) }}</td>
-                    <td>{{ $ride->start }}</td>
-                    <td>{{ $ride->end }}</td>
+                    <td>{{ $ride->start?->format('d-m-Y H:i') }}</td>
+                    <td>{{ $ride->end?->format('d-m-Y H:i') }}</td>
                     <td>
                         <div class="d-flex justify-content-end">
-                            <a href="{{ route('rides.show', ['ride' => $ride->id]) }}" class="btn btn-dark me-2">{{ __('labels.view') }}</a>
-                            <form action="{{ route('rides.destroy', $ride->id) }}" method="POST" onsubmit="return confirm('{{ __('labels.ride_delete_request') }} #{{ $ride->id }}?');">
+                            <a href="{{ route('admin.rides.show', ['ride' => $ride->id]) }}" class="btn btn-dark me-2">{{ __('labels.view') }}</a>
+                            <form action="{{ route('admin.rides.destroy', $ride->id) }}" method="POST" onsubmit="return confirm('{{ __('labels.ride_delete_request') }} #{{ $ride->id }}?');">
                                 @csrf
                                 @method('DELETE')
                                 <button type="submit" class="btn btn-danger">{{ __('labels.delete') }}</button>
